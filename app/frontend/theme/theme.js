28e11d93785b578776359a643a271306c92bffc1
Sylvann <agawinsylvannjules@gmail.com>
2025-09-24 18:22:15 +0800

    Uploaded the app folder components
    This should contain the structure of the application, having a frontend and backend folder

---
+++ app/frontend/theme/theme.js
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#78B16D",
+    secondary: "#FFFFFF",
+    background: "#FFFFFF",
+  },
+};
+
+export default theme;
